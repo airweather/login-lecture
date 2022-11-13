b5f6bfbe0817f7b94f5b9bcad85c426c102440e2
Jun <wnsghd1@naver.com>
2022-11-13 23:34:14 +0900

    dom을 이용하여 로그인 정보 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict"
 
-console.log('hello');
-console.log('bye');
+const id = document.querySelector('#id');
+const password = document.querySelector('#password');
+const loginBtn = document.querySelector('button');
+
+console.log(id, password, loginBtn);
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+
+    console.log(req);
+};
