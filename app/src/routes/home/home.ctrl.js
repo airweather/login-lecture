23a048dd7787c69938ff913750a0fb3f60eae715
Jun <wnsghd1@naver.com>
2022-11-14 03:29:42 +0900

    로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,44 @@
 'use strict';
 
-const home = (req, res) => {
-    res.render('home/index')
-};
 
-const login =  (req, res) => {
-    res.render('home/login')
-};
+const output = {
+    home: (req, res) => {
+        res.render('home/index')
+    },
+    
+    login: (req, res) => {
+        res.render('home/login')
+    },
+}
 
+const users = {
+    id: ['a', 'abc', 'aaaa'],
+    password: ['123', '1234', '12345'],
+}
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id;
+        const password = req.body.password;
+
+        console.log(id, password);
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.password[idx] === password) {
+                return res.json({
+                    success: true,
+                })
+            }
+        }
+        return res.json({
+            success: false,
+            msg: '로그인에 실패하셨습니다',
+        })
+    }
+}
 module.exports = {
     //key가 없으면 hello : hello형태
-    home,
-    login,
+    output,
+    process,
 };
