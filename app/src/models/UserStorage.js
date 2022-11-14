b53fad566f8d92b791e0d6f4b1923177a1e47a0e
Jun <wnsghd1@naver.com>
2022-11-15 06:53:38 +0900

    fs모듈을 통 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,23 @@
 "use strict"
 
+const fs = require('fs').promises;
+
 class UserStorage {
     //# : 변수를 private처럼
-    static #users = {
-        id: ['a', 'abc', 'ccc'],
-        password: ['123', '1234', '12345'],
-        name : ['aaa', 'bbb', 'ccc'],
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); 
+        const userInfo = usersKeys.reduce((newUsers, info) => {
+            newUsers[info] = users[info][idx];
+            return newUsers;
+        }, {}); 
+
+    return userInfo;
     }
 
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -21,19 +29,24 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); 
-        const userInfo = usersKeys.reduce((newUsers, info) => {
-            newUsers[info] = users[info][idx];
-            return newUsers;
-        }, {}); 
+        // const users = this.#users;
+        //promise일 때 .then()은 성공시 실행 .catch는 실패 시
+        return fs
+            .readFile('./src/databases/users.json')
+            .then((data) => {
+               return this.#getUserInfo(data, id);
+            })
+            //err => console.error(err)이지만 파라미터로 넘어온 것을
+            //함수에서 다시 실행하면 생략할 수 있음
+            //즉 (err) => console.error(err)
+            .catch(console.error);
 
-        return userInfo;
     }
 
+    
+
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
