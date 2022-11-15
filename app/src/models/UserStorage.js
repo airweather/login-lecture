e0d3651525076b2ba0524af98c1dc979c1fbcf5f
Jun <wnsghd1@naver.com>
2022-11-16 06:46:20 +0900

    AWS RDS와 연동 & dotenv 환경변수 등록

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,75 +1,48 @@
 "use strict"
 
-const fs = require('fs').promises;
+const db = require('../config/db');
 
 class UserStorage {
     //# : 변수를 private처럼
-    static #getUserInfo(data, id) {
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); 
-        const userInfo = usersKeys.reduce((newUsers, info) => {
-            newUsers[info] = users[info][idx];
-            return newUsers;
-        }, {}); 
-
-    return userInfo;
-    }
-
-    static #getUsers(data, isAll, fields) {
-        const users = JSON.parse(data);
-        if(isAll) return users;
-        // const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        
-        return newUsers;
-    }
-
-    static getUsers(isAll, ...fields) {
-        return fs
-            .readFile('./src/databases/users.json')
-            .then((data) => {
-               return this.#getUsers(data, isAll, fields);
-            })
-            .catch(console.error);
-
-        
-    }
-
+    
     static getUserInfo(id) {
-        // const users = this.#users;
-        //promise일 때 .then()은 성공시 실행 .catch는 실패 시
-        return fs
-            .readFile('./src/databases/users.json')
-            .then((data) => {
-               return this.#getUserInfo(data, id);
+        return new Promise((resolve, reject) => {
+            const query = "SELECT * FROM users WHERE id = ?;";
+            db.query(query, [id] , (err, data) => {
+                if (err) reject(`${err}`);
+                resolve(data[0]);
             })
-            //err => console.error(err)이지만 파라미터로 넘어온 것을
-            //함수에서 다시 실행하면 생략할 수 있음
-            //즉 (err) => console.error(err)
-            .catch(console.error);
-
+        })
     }
+    // static getUserInfo(id) {
+    //     // const users = this.#users;
+    //     //promise일 때 .then()은 성공시 실행 .catch는 실패 시
+    //     return fs
+    //         .readFile('./src/databases/users.json')
+    //         .then((data) => {
+    //            return this.#getUserInfo(data, id);
+    //         })
+    //         //err => console.error(err)이지만 파라미터로 넘어온 것을
+    //         //함수에서 다시 실행하면 생략할 수 있음
+    //         //즉 (err) => console.error(err)
+    //         .catch(console.error);
+
+    // }
 
     
 
     static async save(userInfo) {
-        const users = await this.getUsers(true);
-        if(users.id.includes(userInfo.id)) {
-            throw "이미 존재하는 아이디입니다.";
-        }
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.password.push(userInfo.password);
-        console.log(users);
-        //데이터 추가
-        fs.writeFile('./src/databases/users.json', JSON.stringify(users));
-        return {success:true};
+        return new Promise((resolve, reject) => {
+            const query = "INSERT INTO users(id, name, password) VALUES(?, ?, ?);";
+            db.query(
+                query,
+                [userInfo.id, userInfo.name, userInfo.password],
+                (err) => {
+                    //err는 object이므로 object가 출력되지 않게 변수로...
+                if (err) reject(`${err}`);
+                resolve({success:true});
+            })
+        })
     }
 }
 
